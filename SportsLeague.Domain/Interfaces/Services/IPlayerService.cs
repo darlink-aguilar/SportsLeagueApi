b40f482072d6a6f70a92cb1267b7cd6a49588993
Darlink Aguilar <darlinkaguilargr@gmail.com>
2026-03-24 16:30:39 -0500

    Agregar archivos de proyecto.

---
+++ SportsLeague.Domain/Interfaces/Services/IPlayerService.cs
@@ -0,0 +1,15 @@
+﻿using SportsLeague.Domain.Entities;
+
+namespace SportsLeague.Domain.Interfaces.Services
+{
+    public interface IPlayerService
+    {
+        // Metodos que vamos a configurar en nuestra capa de servicios para manejar la logica de negocio relacionada con los jugadores
+        Task<IEnumerable<Player>> GetAllAsync();
+        Task<Player?> GetByIdAsync(int id);
+        Task<IEnumerable<Player>> GetByTeamAsync(int teamId);
+        Task<Player> CreateAsync(Player player);
+        Task UpdateAsync(int id, Player player);
+        Task DeleteAsync(int id);
+    }
+}
