0f65ea56bf8f901353ebf389ae994e4a5bda705b
Darlink Aguilar <darlinkaguilargr@gmail.com>
2026-04-29 07:46:02 -0500

    DbSet of the match table

--- SportsLeague.Domain/Entities/Team.cs
+++ SportsLeague.Domain/Entities/Team.cs
@@ -8,8 +8,14 @@
         public string? LogoUrl { get; set; }
         public DateTime FoundedDate { get; set; }
 
-        // Navigation Property - Colección de jugadores
+        // Navigation Property
         public ICollection<Player> Players { get; set; } = new List<Player>();
         public ICollection<TournamentTeam> TournamentTeams { get; set; } = new List<TournamentTeam>();
+
+        // Partidos como local
+        public ICollection<Match> HomeMatches { get; set; } = new List<Match>();
+
+        // Partidos como visitante
+        public ICollection<Match> AwayMatches { get; set; } = new List<Match>();
     }
 } 
