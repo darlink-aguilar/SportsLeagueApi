e5ace7e53049368a42138a2a58af3602a260fba5
Darlink Aguilar <darlinkaguilargr@gmail.com>
2026-05-05 13:02:40 -0500

    Comments

--- SportsLeague.Domain/Interfaces/Repositories/ITeamRepository.cs
+++ SportsLeague.Domain/Interfaces/Repositories/ITeamRepository.cs
@@ -5,7 +5,7 @@ namespace SportsLeague.Domain.Interfaces.Repositories
     public interface ITeamRepository : IGenericRepository<Team>
     {
         Task<Team?> GetByNameAsync(string name);
-        Task<IEnumerable<Team>> GetByCityAsync(string city);
+        Task<IEnumerable<Team>> GetByCityAsync(string city); 
     }
 
 }
