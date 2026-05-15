de6b605c4291c9ca6714ac5aa7b848ba8c998935
Darlink Aguilar <darlinkaguilargr@gmail.com>
2026-05-14 21:57:50 -0500

    Creation of the MatchLineup entity

---
+++ SportsLeague.Domain/Entities/MatchLineup.cs
@@ -0,0 +1,16 @@
+﻿using SportsLeague.Domain.Enums;
+
+namespace SportsLeague.Domain.Entities
+{
+    public class MatchLineup : AuditBase
+    {
+        public int MatchId { get; set; }
+        public int PlayerId { get; set; }
+        public bool IsStarter { get; set; } // Indica si el jugador es titular o suplente
+                                            // true = titular, false = suplente
+        public PlayerPosition Position { get; set; } 
+        // Navigation Properties
+        public Match Match { get; set; } = null!;
+        public Player Player { get; set; } = null!;
+    }
+}
